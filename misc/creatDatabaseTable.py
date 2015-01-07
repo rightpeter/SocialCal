2e3a45e06aff6db98159a0e0e2b1017faa18d850
Lu Lihao <lulihao@papayamobile.com>
2015-01-07 10:49:00 +0800

    fix createTable

--- misc/creatDatabaseTable.py
+++ misc/creatDatabaseTable.py
@@ -6,49 +6,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from model import *
 import torndb
 
-
-def installCommentTable():
-    NewsDatabase.reconnect()
-    NewsDatabase.execute("""CREATE TABLE `commTable`(
-            `cid` INT NOT NULL AUTO_INCREMENT, 
-            `id` int, 
-            `level` int, 
-            `tolevel` int, 
-            `content` text, 
-            `posttime` TIMESTAMP, 
-            PRIMARY KEY(cid)) 
-            DEFAULT CHARSET=utf8
-    """)
-
-    
-def installEmailTable():
-    NewsDatabase.reconnect()
-    NewsDatabase.execute("""CREATE TABLE `emailTable`(
-            `id` INT NOT NULL AUTO_INCREMENT,
-            `name` VARCHAR(30),
-            `address` VARCHAR(50),
-            PRIMARY KEY(id))
-    """)
-
-def installNewsTable():
-    NewsDatabase.reconnect()
-    NewsDatabase.execute("""CREATE TABLE `newsTable`(
-            `id` INT NOT NULL AUTO_INCREMENT,
-            `nid` INT NOT NULL,
-            `publisher` VARCHAR(100),
-            `sha1` VARCHAR(100),
-            `date` VARCHAR(100),
-            `title` text,
-            `source` VARCHAR(100),
-            `link` VARCHAR(100),
-            `source_link` VARCHAR(100),
-            `clean_body` text,
-            `body` text,
-            PRIMARY KEY(id))
-            AUTO_INCREMENT=1000000
-            DEFAULT CHARSET=utf8
-    """)
-
 def installUsersTable():
     NewsDatabase.reconnect()
     NewsDatabase.execute("""CREATE TABLE `usersTable`(
@@ -107,14 +64,6 @@ if __name__ == "__main__":
     # db.init_db()
     # models.kv.db_inited = ''
     print sys.argv
-    if '-C' in sys.argv or '-A' in sys.argv:
-        installCommentTable()
-
-    if '-E' in sys.argv or '-A' in sys.argv:
-        installEmailTable()
-
-    if '-N' in sys.argv or '-A' in sys.argv:
-        installNewsTable()
 
     if '-U' in sys.argv or '-A' in sys.argv:
         installUsersTable()
