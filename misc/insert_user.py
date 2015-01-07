240954ad56e5f65905ce16b2027d7534e3b85c8a
Lu Lihao <lulihao@papayamobile.com>
2015-01-07 17:01:11 +0800

    fix

--- misc/insert_user.py
+++ misc/insert_user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import util.myTools as myTools
 
 def insert_rightpeter():
