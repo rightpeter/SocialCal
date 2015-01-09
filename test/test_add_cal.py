6b79a8bd2a5228cc82be3148365447b409c354dd
Lu Lihao <lulihao@papayamobile.com>
2015-01-09 17:09:49 +0800

    fix

--- test/test_add_cal.py
+++ test/test_add_cal.py
@@ -20,7 +20,7 @@ def test_add_240_event_to_rightpeter():
 def test_add_224_event_to_rightpeter():
     user = myTools.get_user_by_name('Rightpeter')
     birthday_event = {}
-    birthday_event['title'] = 'birthday'
+    birthday_event['title'] = '224'
     birthday_event['starttime'] = '2015-06-22 00:00:00'
     birthday_event['endtime'] = '2015-06-22 23:59:59'
     birthday_event['allday'] = 1
@@ -31,7 +31,7 @@ def test_add_224_event_to_rightpeter():
 def test_add_192_event_to_rightpeter():
     user = myTools.get_user_by_name('Rightpeter')
     birthday_event = {}
-    birthday_event['title'] = 'birthday'
+    birthday_event['title'] = '192'
     birthday_event['starttime'] = '2015-06-22 00:00:00'
     birthday_event['endtime'] = '2015-06-22 23:59:59'
     birthday_event['allday'] = 1
@@ -42,7 +42,7 @@ def test_add_192_event_to_rightpeter():
 def test_add_64_event_to_rightpeter():
     user = myTools.get_user_by_name('Rightpeter')
     birthday_event = {}
-    birthday_event['title'] = 'birthday'
+    birthday_event['title'] = '64'
     birthday_event['starttime'] = '2015-06-22 00:00:00'
     birthday_event['endtime'] = '2015-06-22 23:59:59'
     birthday_event['allday'] = 1
@@ -53,7 +53,7 @@ def test_add_64_event_to_rightpeter():
 def test_add_255_event_to_rightpeter():
     user = myTools.get_user_by_name('Rightpeter')
     birthday_event = {}
-    birthday_event['title'] = 'birthday'
+    birthday_event['title'] = '255'
     birthday_event['starttime'] = '2015-06-22 00:00:00'
     birthday_event['endtime'] = '2015-06-22 23:59:59'
     birthday_event['allday'] = 1
@@ -64,7 +64,7 @@ def test_add_255_event_to_rightpeter():
 def test_add_254_event_to_rightpeter():
     user = myTools.get_user_by_name('Rightpeter')
     birthday_event = {}
-    birthday_event['title'] = 'birthday'
+    birthday_event['title'] = '254'
     birthday_event['starttime'] = '2015-06-22 00:00:00'
     birthday_event['endtime'] = '2015-06-22 23:59:59'
     birthday_event['allday'] = 1
@@ -75,7 +75,7 @@ def test_add_254_event_to_rightpeter():
 def test_add_252_event_to_rightpeter():
     user = myTools.get_user_by_name('Rightpeter')
     birthday_event = {}
-    birthday_event['title'] = 'birthday'
+    birthday_event['title'] = '252'
     birthday_event['starttime'] = '2015-06-22 00:00:00'
     birthday_event['endtime'] = '2015-06-22 23:59:59'
     birthday_event['allday'] = 1
@@ -86,7 +86,7 @@ def test_add_252_event_to_rightpeter():
 def test_add_244_event_to_rightpeter():
     user = myTools.get_user_by_name('Rightpeter')
     birthday_event = {}
-    birthday_event['title'] = 'birthday'
+    birthday_event['title'] = '244'
     birthday_event['starttime'] = '2015-06-22 00:00:00'
     birthday_event['endtime'] = '2015-06-22 23:59:59'
     birthday_event['allday'] = 1
