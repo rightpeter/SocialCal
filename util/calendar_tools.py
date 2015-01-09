5ac24929899e3ead3dfdf6e4e29aa1888e87837f
Lu Lihao <lulihao@papayamobile.com>
2015-01-09 14:47:04 +0800

    fix

--- util/calendar_tools.py
+++ util/calendar_tools.py
@@ -5,12 +5,10 @@ from model import *
 from config import *
 
 def add_event_to_user(event, user):
-    content = '''INSERT INTO `calendarTable`(
-        hid, title, starttime, endtime, allday) VALUE(%s, %s, "%s", "%s", %s)
-        ''' % (user['id'], event['title'], event['starttime'],
+    CalendarDatabase.execute('''INSERT INTO `calendarTable`(
+        hid, title, starttime, endtime, allday) VALUE(%s, %s, %s, %s, %s)
+        ''', user['id'], event['title'], event['starttime'],
             event['endtime'], event['allday'])
-    print 'content: ', content
-    CalendarDatabase.execute(content)
     return True
 
 def get_event_of_user(user):
