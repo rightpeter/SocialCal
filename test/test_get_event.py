52d1f6728c550b696a9c08dda9a622741a2774af
Lu Lihao <lulihao@papayamobile.com>
2015-01-09 16:34:25 +0800

    fix

--- test/test_get_event.py
+++ test/test_get_event.py
@@ -6,7 +6,14 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import util.myTools as myTools
 import util.calendar_tools as calendar_tools
 
-def test_get_event():
+def test_get_events():
+    print '------------ test_get_events -------------'
+    user = myTools.get_user_by_name('Rightpeter')
+    events = calendar_tools.get_event_of_user(user)
+    print 'events: ', events
+
+def test_get_events_to_guest():
+    print '------------ test_get_events_to_guest ----------'
     user = myTools.get_user_by_name('Rightpeter') 
     guest = myTools.get_user_by_name('june_fiend')
     events = calendar_tools.get_event_of_user_to_guest(user, guest, 1)
