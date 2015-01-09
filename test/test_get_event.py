c5d2002dc7152721cf7331d1e9e464f5dd564345
Lu Lihao <lulihao@papayamobile.com>
2015-01-09 16:27:41 +0800

    fix

---
+++ test/test_get_event.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import util.myTools as myTools
+import util.calendar_tools as calendar_tools
+
+def test_get_event():
+    user = myTools.get_user_by_name('Rightpeter') 
+    guest = myTools.get_user_by_name('june_fiend')
+    events = calendar_tools.get_event_of_user_to_guest(user, guest, 1)
+    print 'events: ', events
+
+if __name__ == '__main__':
+    test_get_event()
