650d3b1feeae1581027add15d940dd46f655176a
Lu Lihao <lulihao@papayamobile.com>
2015-01-12 10:40:36 +0800

    fix

--- test/test_share_a_event.py
+++ test/test_share_a_event.py
@@ -7,12 +7,13 @@ import util.myTools as myTools
 import util.calendar_tools as calendar_tools
 
 def test_share_a_event():
+    rel = int(raw_input('Input the rel of guest: \n'))
     user = myTools.get_user_by_name('Rightpeter')
     guest = myTools.get_user_by_name('june_fiend')
-    events = calendar_tools.get_events_of_user_to_guest(user, guest, 1)
+    events = calendar_tools.get_events_of_user_to_guest(user, guest, rel)
     print 'events: ', events
     for event in events:
-        event_info = calendar_tools.get_event_by_id(event['id'], guest, 1)
+        event_info = calendar_tools.get_event_by_id(event['id'], guest, rel)
         try:
             print 'id: ', event_info.id, ' title: ', event_info.title, ' privilege: ', event_info.privilege
         except Exception, e:
@@ -20,18 +21,16 @@ def test_share_a_event():
             print 'None'
 
     eid = int(raw_input('Input the ID of event: \n'))
-    rel = int(raw_input('Input the rel of guest: \n'))
     res = calendar_tools.share_a_event(guest, eid, rel)
     print res
     events = calendar_tools.get_events_of_user(guest)
     print 'events: ', events
     for event in events:
-        event_info = calendar_tools.get_event_by_id(event['id'], guest, 1)
+        event_info = calendar_tools.get_event_by_id(event['id'], guest, rel)
         try:
             print 'id: ', event_info.id, ' title: ', event_info.title, ' privilege: ', event_info.privilege
         except Exception, e:
             print e
-            print 'None'
 
 if __name__ == '__main__':
     test_share_a_event()
